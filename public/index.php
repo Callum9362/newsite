9a24920717cb801c05c989ffe9c8a0d79a0bebc5
Callum9362 <callum.gill.1993.cg@gmail.com>
2020-12-18 18:01:06 +1100

    Added sections for the newsite

--- public/index.php
+++ public/index.php
@@ -1,7 +1,45 @@
-<?php include 'header.php';?>
+<?php include 'header.php'; ?>
 
 <body>
-    <h1 class="text-center">This is the homepage</h1>
+
+    <div class="container pt-5 pb-5">
+        <div class="card">
+            <div class="card-body text-center">
+                <h3>Premium & Cost-effective Distribution Channels</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis rem vel assumenda ipsam eum incidunt, obcaecati, deleniti fuga voluptatem quisquam accusantium magni vitae aliquam cum est nobis dolores eius non.
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi a exercitationem reiciendis soluta repudiandae. Voluptatibus maiores dicta omnis ad deserunt animi, accusamus reiciendis facere iste ab, alias blanditiis quos dolorum?
+                </p>
+                <button type="button" class="btn btn-warning btn-lg">Submit Press Release</button>
+                <div class="row pt-2">
+                    <div class="col-sm">
+                        <h5>555-666-67788</h5>
+                        <button class="btn btn-primary" type="submit"><i class="fa fa-phone" aria-hidden="true"></i> Give Us a Call</button>
+                    </div>
+                    <div class="col-sm">
+                        <h5>newssite@newshub.com</h5>
+                        <button class="btn btn-primary" type="submit"><i class="fa fa-envelope-o" aria-hidden="true"></i> Email Us</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="container pb-5">
+        <div class="card">
+            <div class="card-body text-center">
+                <h3>InvestorsHub NewsWire Services</h3>
+            </div>
+        </div>
+    </div>
+
+    <div class="container pb-5">
+        <div class="card">
+            <div class="card-body text-center">
+                <h3>News Room</h3>
+            </div>
+        </div>
+    </div>
+
 </body>
 
-<?php include 'footer.php';?>
+<?php include 'footer.php'; ?>
