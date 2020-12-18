041099738851971778875429b80aafef236cbed6
Callum9362 <callum.gill.1993.cg@gmail.com>
2020-12-18 17:47:26 +1100

    Added the contact page

--- public/contact.php
+++ public/contact.php
@@ -1,7 +1,17 @@
-<?php include 'header.php';?>
+<?php include 'header.php'; ?>
 
 <body>
-    <h1 class="text-center">This is the contact page</h1>
+    <div class="container pt-5 pb-5">
+        <div class="card">
+            <div class="card-header">
+                <h1 class="text-center">Newsite Contact Information</h1>
+            </div>
+            <div class="card-body">
+                <h3 class="text-center pb-3"><i class="fa fa-phone" aria-hidden="true"></i>  555-666-67788</h3>
+                <h3 class="text-center"><i class="fa fa-envelope-o" aria-hidden="true"></i>  newssite@newshub.com</h3>
+            </div>
+        </div>
+    </div>
 </body>
 
-<?php include 'footer.php';?>
+<?php include 'footer.php'; ?>
--- public/footer.php
+++ public/footer.php
@@ -8,10 +8,19 @@
         </div>
         <div class="col-sm">
           <ul class="list-unstyled">
-            <li><i class="icon-github"></i><a href="https://github.com/Callum9362">Github</a></li>
-            <li><i class="fa-linkedin"></i><a href="https://www.linkedin.com/in/callum-gill-9b753ba7/">Linkedin</a></li>
+            <li><i class=""></i><a href="https://github.com/Callum9362">Github</a></li>
+            <li><i class=""></i><a href="https://www.linkedin.com/in/callum-gill-9b753ba7/">Linkedin</a></li>
           </ul>
         </div>
       </div>
     </div>
+    <!-- jQuery first, then Bootstrap JS. -->
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
+    integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
+    crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx"
+    crossorigin="anonymous"></script>
+    <script src="https://use.fontawesome.com/48f57dd94f.js"></script>
   </footer>
