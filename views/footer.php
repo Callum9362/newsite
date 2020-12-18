4674cf8432f21c16ca0a50be21693842364f8f2b
Callum9362 <callum.gill.1993.cg@gmail.com>
2020-12-18 17:21:27 +1100

    Added in the footer

---
+++ views/footer.php
@@ -0,0 +1,17 @@
+<footer id="footer-main">
+    <div class="container">
+      <div class="row">
+        <div class="col-sm">
+          <p>Coded by Callum9362</p>
+          <p>Copyright 2020</p>
+          <p>Email: callum.gill1993@hotmail.co.uk</p>
+        </div>
+        <div class="col-sm">
+          <ul class="list-unstyled">
+            <li><i class="icon-github"></i><a href="https://github.com/Callum9362">Github</a></li>
+            <li><i class="fa-linkedin"></i><a href="https://www.linkedin.com/in/callum-gill-9b753ba7/">Linkedin</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </footer>
--- views/index.php
+++ views/index.php
@@ -1,1 +1,7 @@
 <?php include 'header.php';?>
+
+<body>
+    <h1 class="text-center">This is the homepage</h1>
+</body>
+
+<?php include 'footer.php';?>
