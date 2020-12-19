f5608e3d80f9b2e5d74c4055d45b24a2e79ef761
Callum9362 <callum.gill.1993.cg@gmail.com>
2020-12-19 13:31:54 +1100

    Added in join now script need to investigate why its not inserting into db table

--- public/header.php
+++ public/header.php
@@ -1,3 +1,7 @@
+<?php
+require_once '../database/Dbconfig.php';
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
--- public/join.php
+++ public/join.php
@@ -1,4 +1,60 @@
-<?php include 'header.php'; ?>
+<?php 
+
+include 'header.php'; 
+
+
+if(isset($_POST['btn-signup']))
+{
+   $uname = trim($_POST['txt_uname']);
+   $umail = trim($_POST['txt_umail']);
+   $upass = trim($_POST['txt_upass']); 
+ 
+   if($uname=="") {
+      $error[] = "provide username !"; 
+   }
+   else if($umail=="") {
+      $error[] = "provide email id !"; 
+   }
+   else if(!filter_var($umail, FILTER_VALIDATE_EMAIL)) {
+      $error[] = 'Please enter a valid email address !';
+   }
+   else if($upass=="") {
+      $error[] = "provide password !";
+   }
+   else if(strlen($upass) < 6){
+      $error[] = "Password must be atleast 6 characters"; 
+   }
+   else
+   {
+      try
+      {
+         $stmt = $DB_con->prepare("SELECT user_name,user_email FROM users WHERE user_name=:uname OR user_email=:umail");
+         $stmt->execute(array(':uname'=>$uname, ':umail'=>$umail));
+         $row=$stmt->fetch(PDO::FETCH_ASSOC);
+    
+         if($row['user_name']==$uname) {
+            $error[] = "sorry username already taken !";
+         }
+         else if($row['user_email']==$umail) {
+            $error[] = "sorry email id already taken !";
+         }
+         else
+         {
+            if($user->register($fname,$lname,$uname,$umail,$upass)) 
+            {
+                $user->redirect('join.php?joined');
+            }
+         }
+     }
+     catch(PDOException $e)
+     {
+        echo $e->getMessage();
+     }
+  } 
+}
+
+?>
+
 
 <body>
     <div class="container pt-5 pb-5">
@@ -7,25 +63,41 @@
                 <h1 class="text-center">Sign Up Today</h1>
             </div>
             <div class="card-body">
-                <form>
+                <form method="POST">
+                <?php
+            if(isset($error))
+            {
+               foreach($error as $error)
+               {
+                  ?>
+                  <div class="alert alert-danger">
+                      <i class="glyphicon glyphicon-warning-sign"></i> &nbsp; <?php echo $error; ?>
+                  </div>
+                  <?php
+               }
+            }
+            else if(isset($_GET['joined']))
+            {
+                ?>
+                <div class="alert alert-info">
+                     <i class="glyphicon glyphicon-log-in"></i> &nbsp; Successfully registered <a href='index.php'>login</a> here
+                </div>
+                <?php
+           }
+           ?>
                     <div class="form-group">
-                        <label for="exampleInputEmail1">Email address</label>
-                        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-                        <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+                        <label for="exampleInputEmail1">Username</label>
+                        <input type="text" class="form-control" id="txt_uname">
+                    </div>
+                    <div class="form-group">
+                        <label for="exampleInputEmail1">Email</label>
+                        <input type="email" class="form-control" id="txt_email" aria-describedby="emailHelp">
                     </div>
                     <div class="form-group">
                         <label for="exampleInputPassword1">Password</label>
-                        <input type="password" class="form-control" id="exampleInputPassword1">
+                        <input type="password" class="form-control" id="txt_upass">
                     </div>
-                    <div class="form-group">
-                        <label for="exampleInputPassword1">Confirm Password</label>
-                        <input type="password" class="form-control" id="exampleInputPassword1">
-                    </div>
-                    <div class="form-group form-check">
-                        <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                        <label class="form-check-label" for="exampleCheck1">Terms and Conditions</label>
-                    </div>
-                    <button type="submit" class="btn btn-primary">Submit</button>
+                    <button type="submit" id="btn-signup" class="btn btn-primary">Submit</button>
                 </form>
             </div>
         </div>
