4629dfe484597964a6c7500ca9ac960aac5efa2e
Callum9362 <callum.gill.1993.cg@gmail.com>
2020-12-20 14:55:38 +1100

    Added login functionality

--- public/join.php
+++ public/join.php
@@ -1,110 +1,96 @@
-<?php 
+<?php
 
-include 'header.php'; 
+include 'header.php';
 
 
 
-if(isset($_POST['btn-signup']))
-{
+if (isset($_POST['btn-signup'])) {
    $uname = trim($_POST['txt_uname']);
    $umail = trim($_POST['txt_umail']);
-   $upass = trim($_POST['txt_upass']); 
- 
-   if($uname=="") {
-      $error[] = "provide username !"; 
-   }
-   else if($umail=="") {
-      $error[] = "provide email id !"; 
-   }
-   else if(!filter_var($umail, FILTER_VALIDATE_EMAIL)) {
+   $upass = trim($_POST['txt_upass']);
+
+   if ($uname == "") {
+      $error[] = "provide username !";
+   } else if ($umail == "") {
+      $error[] = "provide email id !";
+   } else if (!filter_var($umail, FILTER_VALIDATE_EMAIL)) {
       $error[] = 'Please enter a valid email address !';
-   }
-   else if($upass=="") {
+   } else if ($upass == "") {
       $error[] = "provide password !";
-   }
-   else if(strlen($upass) < 6){
-      $error[] = "Password must be atleast 6 characters"; 
-   }
-   else
-   {
-      try
-      {
+   } else if (strlen($upass) < 6) {
+      $error[] = "Password must be atleast 6 characters";
+   } else {
+      try {
          $stmt = $DB_con->prepare("SELECT user_name,user_email FROM users WHERE user_name=:uname OR user_email=:umail");
-         $stmt->execute(array(':uname'=>$uname, ':umail'=>$umail));
-         $row=$stmt->fetch(PDO::FETCH_ASSOC);
-    
-         if($row['user_name']==$uname) {
+         $stmt->execute(array(':uname' => $uname, ':umail' => $umail));
+         $row = $stmt->fetch(PDO::FETCH_ASSOC);
+
+         if ($row['user_name'] == $uname) {
             $error[] = "sorry username already taken !";
-         }
-         else if($row['user_email']==$umail) {
+         } else if ($row['user_email'] == $umail) {
             $error[] = "sorry email id already taken !";
-         }
-         else
-         {
-            if($user->register($uname,$umail,$upass)) 
-            {
-                $user->redirect('join.php');
+         } else {
+            if ($user->register($uname, $umail, $upass)) {
+               $user->redirect('join.php');
             }
          }
-     }
-     catch(PDOException $e)
-     {
-        echo $e->getMessage();
-     }
-  } 
+      } catch (PDOException $e) {
+         echo $e->getMessage();
+      }
+   }
 }
 
 ?>
 
 
 <body>
-    <div class="container pt-5 pb-5">
-        <div class="card">
-            <div class="card-header">
-                <h1 class="text-center">Sign Up Today</h1>
-            </div>
-            <div class="card-body">
-                <form method="post">
-                <?php
-            if(isset($error))
-            {
-               foreach($error as $error)
-               {
-                  ?>
-                  <div class="alert alert-danger">
-                      <i class="glyphicon glyphicon-warning-sign"></i> &nbsp; <?php echo $error; ?>
-                  </div>
+   <div class="container pt-5 pb-5">
+      <div class="card">
+         <div class="card-header">
+            <h1 class="text-center">Sign Up Today</h1>
+         </div>
+         <div class="card-body">
+            <form method="post">
+               <?php
+               if (isset($error)) {
+                  foreach ($error as $error) {
+               ?>
+                     <div class="alert alert-danger">
+                        <i class="glyphicon glyphicon-warning-sign"></i> &nbsp; <?php echo $error; ?>
+                     </div>
                   <?php
-               }
-            }
-            else if(isset($_GET['joined']))
-            {
-                ?>
-                <div class="alert alert-info">
+                  }
+               } else if (isset($_GET['joined'])) {
+                  ?>
+                  <div class="alert alert-info">
                      <i class="glyphicon glyphicon-log-in"></i> &nbsp; Successfully registered <a href='login.php'>login</a> here
-                </div>
-                <?php
-           }
-           ?>
-                    <div class="form-group">
-                        <label for="exampleInputEmail1">Username</label>
-                        <input type="text" class="form-control" name="txt_uname" value="<?php if(isset($error)){echo $uname;}?>">
-                    </div>
-                    <div class="form-group">
-                        <label for="exampleInputEmail1">Email</label>
-                        <input type="email" class="form-control" name="txt_umail" aria-describedby="emailHelp" value="<?php if(isset($error)){echo $umail;}?>">
-                    </div>
-                    <div class="form-group">
-                        <label for="exampleInputPassword1">Password</label>
-                        <input type="password" class="form-control" name="txt_upass">
-                    </div>
-                    <div class="text-center">
-                     <button type="submit" name="btn-signup" class="btn btn-primary btn-lg">Register</button>
-                    </div>
-                </form>
-            </div>
-        </div>
-    </div>
+                  </div>
+               <?php
+               }
+               ?>
+               <div class="form-group">
+                  <label for="exampleInputEmail1">Username</label>
+                  <input type="text" class="form-control" name="txt_uname" value="<?php if (isset($error)) {
+                                                                                       echo $uname;
+                                                                                    } ?>">
+               </div>
+               <div class="form-group">
+                  <label for="exampleInputEmail1">Email</label>
+                  <input type="email" class="form-control" name="txt_umail" aria-describedby="emailHelp" value="<?php if (isset($error)) {
+                                                                                                                     echo $umail;
+                                                                                                                  } ?>">
+               </div>
+               <div class="form-group">
+                  <label for="exampleInputPassword1">Password</label>
+                  <input type="password" class="form-control" name="txt_upass">
+               </div>
+               <div class="text-center">
+                  <button type="submit" name="btn-signup" class="btn btn-primary btn-lg">Register</button>
+               </div>
+            </form>
+         </div>
+      </div>
+   </div>
 </body>
 
 <?php include 'footer.php'; ?>
--- public/login.php
+++ public/login.php
@@ -1,4 +1,23 @@
-<?php include 'header.php';?>
+<?php
+
+include 'header.php';
+
+if ($user->is_loggedin() != "") {
+    $user->redirect('index.php');
+}
+
+if (isset($_POST['btn-login'])) {
+    $uname = $_POST['txt_uname_email'];
+    $umail = $_POST['txt_uname_email'];
+    $upass = $_POST['txt_password'];
+
+    if ($user->login($uname, $umail, $upass)) {
+        $user->redirect('index.php');
+    } else {
+        $error = "Wrong Details !";
+    }
+}
+?>
 
 <body>
     <div class="container pt-5 pb-5">
@@ -7,17 +26,26 @@
                 <h1 class="text-center">Login</h1>
             </div>
             <div class="card-body">
+                <?php
+                if (isset($error)) {
+                ?>
+                    <div class="alert alert-danger">
+                        <i class="glyphicon glyphicon-warning-sign"></i> &nbsp; <?php echo $error; ?> !
+                    </div>
+                <?php
+                }
+                ?>
                 <form method="POST">
                     <div class="form-group">
                         <label for="exampleInputEmail1">Email</label>
-                        <input type="email" class="form-control" id="txt_email" aria-describedby="emailHelp">
+                        <input type="text" class="form-control" name="txt_uname_email" aria-describedby="emailHelp">
                     </div>
                     <div class="form-group">
                         <label for="exampleInputPassword1">Password</label>
-                        <input type="password" class="form-control" id="txt_upass">
+                        <input type="password" class="form-control" name="txt_password">
                     </div>
                     <div class="text-center">
-                      <button type="submit" id="btn-signup" class="btn btn-success btn-lg btn-block">Login</button>
+                        <button type="submit" name="btn-login" class="btn btn-success btn-lg btn-block">Login</button>
                     </div>
                 </form>
             </div>
@@ -25,4 +53,4 @@
     </div>
 </body>
 
-<?php include 'footer.php';?>
+<?php include 'footer.php'; ?>
