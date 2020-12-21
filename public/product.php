0cbc46e73e0809a3f11c6c695e90d8a308a0c607
Callum9362 <callum.gill.1993.cg@gmail.com>
2020-12-22 10:19:23 +1100

    Enhanced the product page

--- public/product.php
+++ public/product.php
@@ -1,7 +1,40 @@
-<?php include 'header.php';?>
+<?php include 'header.php'; ?>
 
 <body>
-    <h1 class="text-center">This is the products page</h1>
+    <h1 class="text-center">Products Available on Newsite</h1>
+    <div class="container">
+        <table class="table table-bordered">
+            <thead>
+                <tr>
+                    <th scope="col">Name</th>
+                    <th scope="col">Description</th>
+                    <th scope="col">Price</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <th scope="row">Website</th>
+                    <td>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum aliquid soluta sint numquam molestiae sequi, pariatur provident placeat ratione totam commodi, molestias ab perspiciatis nobis quia aspernatur. Voluptatibus, aut minima!</td>
+                    <td><h6>$500</h6></td>
+                </tr>
+                <tr>
+                    <th scope="row">OtcMarkProd</th>
+                    <td>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum aliquid soluta sint numquam molestiae sequi, pariatur provident placeat ratione totam commodi, molestias ab perspiciatis nobis quia aspernatur. Voluptatibus, aut minima!</td>
+                    <td><h6>$50</h6></td>
+                </tr>
+                <tr>
+                    <th scope="row">NettyNetworkNews</th>
+                    <td>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi voluptatibus quo fugiat molestias inventore veritatis, optio, placeat iusto dolores maiores repellendus blanditiis, natus itaque exercitationem officia id iste deserunt nesciunt!</td>
+                    <td><h6>$500</h6></td>
+                </tr>
+                <tr>
+                    <th scope="row">ICanNews</th>
+                    <td>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi voluptatibus quo fugiat molestias inventore veritatis, optio, placeat iusto dolores maiores repellendus blanditiis, natus itaque exercitationem officia id iste deserunt nesciunt!</td>
+                    <td><h6>$100</h6></td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
 </body>
 
-<?php include 'footer.php';?>
+<?php include 'footer.php'; ?>
